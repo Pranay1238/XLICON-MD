db831244c4f80529f21aa82d1e6305799f7ae524
Pranay1238 <154003125+Pranay1238@users.noreply.github.com>
2025-01-22 15:22:01 +0530

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,18 +1,10 @@
-{ pkgs }: {
-    deps = [
-        pkgs.nodejs
-        pkgs.nodePackages.typescript
-        pkgs.imagemagick
-        pkgs.yarn
-        pkgs.git
-        pkgs.ffmpeg
-        pkgs.neofetch
-        pkgs.libwebp
-        pkgs.speedtest-cli
-        pkgs.wget
-        pkgs.libuuid
-    ]; 
-      env ={
-    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid];
-  };
-} 
+{
+  "SESSION_ID": "ID-Here",
+  "OWNER_NUMBER": "92xxxxxxxxxx",
+  "OWNER_NAME": "SalmanAhmad",
+  "OPENAI_API_KEY": "null",
+  "MONGODB_URI": "required",
+  "PACK_INFO": "WABOT;Xlicon",
+  "PREFIX": "."
+   
+}
